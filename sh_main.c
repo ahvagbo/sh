366d14937470204a11f917f30f8d9979402ceb92
Iurie Jurja <iuriejurja@hotmail.com>
2026-02-15 14:07:29 +0200

    Modify several files
    * This adds the GNU GPL v3 notices at the start of each C source file and the Makefile.
    * Modify sh_main.c to include the GPL start notice.

--- sh_main.c
+++ sh_main.c
@@ -1,3 +1,21 @@
+/* sh_main.c - The main part of the shell. */
+/* Copyright (C) 2026 Iurie Jurja */
+
+/*
+ * This file is part of Latbase Shell.
+ *
+ * Latbase Shell is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Latbase Shell is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Latbase Shell. If not,
+ * see <https://www.gnu.org/licenses/>/
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -10,13 +28,18 @@
 int main(int argc, char* argv[]) {
 	sh_load_configs();
 
-	fprintf(stdout, "Welcome to Latbase shell!\n");
-	fprintf(stdout, "You are currently running a PRE-RELEASE version of the program, so\n");
-	fprintf(stdout, "feel free to contribute by adding new features, improvise come code\n");
-	fprintf(stdout, "here and there, so don't hesitate to do so!\n");
-	fprintf(stdout, "Licensed under the terms and conditions of the GNU General Public License\n");
-	fprintf(stdout, "version 3.0\n");
-	fprintf(stdout, "Copyright (C) 2026 Iurie Jurja\n");
+	fprintf(stdout, "Latbase Shell, under construction\n");
+	fprintf(stdout, "Copyright (C) 2026 Iurie Jurja\n\n");
+	fprintf(stdout, "This program is free software: you can redistribute it and/or modify\n");
+	fprintf(stdout, "it under the terms of the GNU General Public License as published by\n");
+	fprintf(stdout, "the Free Software Foundation, either version 3 of the License, or\n");
+	fprintf(stdout, "(at your option) any later version.\n\n");
+	fprintf(stdout, "This program is distributed in the hope that it will be useful,\n");
+	fprintf(stdout, "but WITHOUT ANY WARRANTY; without even the implied warranty of\n");
+	fprintf(stdout, "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n");
+	fprintf(stdout, "GNU General Public License for more details.\n\n");
+	fprintf(stdout, "You should have received a copy of the GNU General Public License\n");
+	fprintf(stdout, "along with this program. If not, see <https://www.gnu.org/licenses/>\n");
 	fprintf(stdout, "\n");
 
 	while (true) {
