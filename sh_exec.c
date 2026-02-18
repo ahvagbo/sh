ecece68347a1eedde57de42c034fe6acf172535a
Iurie Jurja <iuriejurja@hotmail.com>
2026-02-18 17:02:39 +0200

    Modify sh_exec.c
    * Add support for running external commands, not just the built-in ones.

--- sh_exec.c
+++ sh_exec.c
@@ -19,13 +19,19 @@
 #include "sh_exec.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
 
 int sh_exec(int argc, char* argv[]) {
 	int i;
 
-	if (argv[0] == NULL) {
+	if (argv == NULL || argv[0] == NULL) {
 		return 1;
 	}
 
@@ -35,6 +41,63 @@ int sh_exec(int argc, char* argv[]) {
 		}
 	}
 
-	return 0;
+	pid_t pid = fork();
+	if (pid < 0) {
+		perror("fork");
+		return -1;
+	}
+
+	if (pid == 0) {
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		execvp(argv[0], argv);
+
+		int err = errno;
+
+		if (err == ENOENT) {
+			fprintf(stderr, "%s: command not found\n", argv[0]);
+		} else if (err == EACCES) {
+			fprintf(stderr, "%s: permission denied\n", argv[0]);
+		} else {
+			fprintf(stderr, "%s: %s\n", argv[0], strerror(err));
+		}
+
+		_exit(127);
+	}
+
+	int status = 0;
+	struct sigaction ignore_action, old_int_action, old_quit_action;
+	memset(&ignore_action, 0, sizeof(ignore_action));
+	ignore_action.sa_handler = SIG_IGN;
+	sigemptyset(&ignore_action.sa_mask);
+	ignore_action.sa_flags = 0;
+
+	sigaction(SIGINT, &ignore_action, &old_int_action);
+	sigaction(SIGQUIT, &ignore_action, &old_quit_action);
+
+	while (true) {
+		pid_t w = waitpid(pid, &status, 0);
+		if (w == -1) {
+			if (errno == EINTR) continue;
+			perror("waitpid");
+			status = -1;
+			break;
+		}
+		break;
+	}
+
+	sigaction(SIGINT, &old_int_action, NULL);
+	sigaction(SIGQUIT, &old_quit_action, NULL);
+
+	if (status == -1) return -1;
+
+	if (WIFEXITED(status)) {
+		return WEXITSTATUS(status);
+	} else if (WIFSIGNALED(status)) {
+		int sig = WTERMSIG(status);
+		return 128 + sig;
+	}
+
+	return status;
 }
 
